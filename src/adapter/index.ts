1d9a560fb656088108d2aafa7994bd9248cf95a4
Ondřej Maxa <ondrej.maxa11@gmail.com>
2025-02-01 16:09:58 +0100

    chore: remove console log

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -14,7 +14,6 @@ export const effectHandler =
 	({ context, plugin }) => {
 		const ctx = ApiDevContext.layer(context, plugin);
 		const logLevel = detectLogLevel(context.config.logs.level);
-		console.log(logLevel);
 		const appLayer = Layer.mergeAll(
 			Logger.pretty,
 			Logger.minimumLogLevel(logLevel),
