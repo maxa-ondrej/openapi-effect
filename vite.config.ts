3db3467324882b4b636428a1a1fab031fbf34751
Ondřej Maxa <ondrej.maxa11@gmail.com>
2025-02-01 17:33:20 +0100

    chore: bundle the app but without external

--- vite.config.ts
+++ vite.config.ts
@@ -9,31 +9,35 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
 	plugins: [dts({ outDir: 'dist/dts', tsconfigPath: 'tsconfig.src.json' })],
 	build: {
-		minify: false,
 		sourcemap: true,
+		lib: {
+			entry: 'src/index.ts',
+			name: 'openapi-effect',
+			formats: ['es', 'cjs'],
+		},
 		rollupOptions: {
 			external: ['effect', '@effect/platform', 'typescript'],
-			input: Object.fromEntries(
-				globSync('src/**/*.ts').map((file) => [
-					path.relative(
-						'src',
-						file.slice(0, file.length - path.extname(file).length),
-					),
-					fileURLToPath(new URL(file, import.meta.url)),
-				]),
-			),
-			output: [
-				{
-					format: 'cjs',
-					dir: 'dist/cjs',
-					entryFileNames: 'src/[name].js',
-				},
-				{
-					format: 'esm',
-					dir: 'dist/esm',
-					entryFileNames: 'src/[name].js',
-				},
-			],
+			// input: Object.fromEntries(
+			// 	globSync('src/**/*.ts').map((file) => [
+			// 		path.relative(
+			// 			'src',
+			// 			file.slice(0, file.length - path.extname(file).length),
+			// 		),
+			// 		fileURLToPath(new URL(file, import.meta.url)),
+			// 	]),
+			// ),
+			// output: [
+			// 	{
+			// 		format: 'cjs',
+			// 		dir: 'dist/cjs',
+			// 		entryFileNames: 'src/[name].js',
+			// 	},
+			// 	{
+			// 		format: 'esm',
+			// 		dir: 'dist/esm',
+			// 		entryFileNames: 'src/[name].js',
+			// 	},
+			// ],
 		},
 	},
 	test: {
